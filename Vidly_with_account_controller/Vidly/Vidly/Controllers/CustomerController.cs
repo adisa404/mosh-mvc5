ec51afa52a8a9004106c9e6db5dde2b3531a5cdd
Adisa Mrvoljak <adisa.mrvoljak@live.com>
2018-11-25 15:50:02 +0100

    first exercise: get all customers with action links and edit pages. get all movies without links. set bootstrap-lumen theme (new project)

---
+++ Vidly_with_account_controller/Vidly/Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+			var customers = GetCustomers();
+
+			return View(customers);
+		}
+
+		public ActionResult Edit(int? id)
+		{
+			if (!id.HasValue) return HttpNotFound();
+
+			var customer = GetCustomers().Where(c => c.Id == id).FirstOrDefault();
+			return View(customer);
+		}
+
+		private IEnumerable<Customer> GetCustomers()
+		{
+			return new List<Customer>() {
+				new Customer { Name = "Customer 1", Id = 1},
+				new Customer { Name = "Customer 2", Id = 2}
+			};
+		}
+	}
+}
