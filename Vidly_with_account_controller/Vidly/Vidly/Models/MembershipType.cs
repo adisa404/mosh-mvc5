6c96f1955acbbe2b3c67c30f831872cdfdbb0ac3
Adisa Mrvoljak <adisa.mrvoljak@live.com>
2018-11-26 10:56:25 +0100

    new migration - add IsSubscribedToNewsletter to customer. Added new class MembershipType

---
+++ Vidly_with_account_controller/Vidly/Vidly/Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+	public class MembershipType
+	{
+		public double SignUpFee { get; set; }
+
+		public short Duration { get; set; }
+
+		public short DiscountRate { get; set; }
+	}
+}
