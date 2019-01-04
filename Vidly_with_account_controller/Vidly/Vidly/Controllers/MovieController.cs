5bcedd725b98623c68292b53f8aa0f3d93506f86
Adisa Mrvoljak <adisa.mrvoljak@live.com>
2019-01-04 23:03:06 +0100

    GetMovies over db + extend Movie model + populate views Index and Details + add Genre model + add migration for extending Movie model and adding genre model

--- Vidly_with_account_controller/Vidly/Vidly/Controllers/MovieController.cs
+++ Vidly_with_account_controller/Vidly/Vidly/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,49 +11,42 @@ namespace Vidly.Controllers
 {
 	public class MovieController : Controller
 	{
+		private ApplicationDbContext _context;
+
+		public MovieController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
 		// GET: Movies
 		public ActionResult Index()
 		{
-			var movies = GetMovies();
+			var movies = 
+				//get list of movies in Index view
+				_context.Movies.Include(m => m.Genres).ToList();
 			return View(movies);
 		}
 
-		// GET: Movies/Random
-		public ActionResult Random()
+		
+
+		public ActionResult Details(int id)
 		{
-			var movie = new Movie() { Name = "Harry Potter!" };
-
-			var customers = new List<Customer>() {
-				new Customer { Name = "Customer 1" },
-				new Customer { Name = "Customer 2" }
-			};
-
-			var viewModel = new RandomMovieViewModel() { Customers = customers, Movie = movie };
-			return View(viewModel);
-			//return Content("text");
-			//return HttpNotFound();
-			//return new EmptyResult(); // prazna stranica
-			//return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
-		}
-
-		public ActionResult Edit(int id)
-		{
-			return View();
-			// Content("id=" + id);
+			var movieById = _context
+				.Movies
+				.Include(m => m.Genres)
+				.SingleOrDefault(m => m.Id == id);
+			return View(movieById);
 		}
 
 		[Route("movie/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
 		public ActionResult ByReleaseDate(int year, int month)
 		{
 			return Content(String.Format("{0}/{1}", year, month));
-		}
-
-		private IEnumerable<Movie> GetMovies()
-		{
-			return new List<Movie>() {
-				new Movie {Name = "Harry Potter" },
-				new Movie { Name = "Shrek"}
-			};
 		}
 	}
 }
--- Vidly_with_account_controller/Vidly/Vidly/Models/Movie.cs
+++ Vidly_with_account_controller/Vidly/Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,5 +10,13 @@ namespace Vidly.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
+
+		[Required]
+		public Genre Genres { get; set; }
+		public int GenreId { get; set; }
+
+		public DateTime DateAdded { get; set; }
+		public DateTime DateReleased { get; set; }
+		public int NumberInStock { get; set; }
 	}
 }
