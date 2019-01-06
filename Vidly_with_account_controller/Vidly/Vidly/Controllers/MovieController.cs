874a218e840e3fb21856528a7a8e2170b9a13d9f
Adisa Mrvoljak <adisa.mrvoljak@live.com>
2019-01-06 01:26:30 +0100

    section 4 - exercise - add form for new/edit movie

--- Vidly_with_account_controller/Vidly/Vidly/Controllers/MovieController.cs
+++ Vidly_with_account_controller/Vidly/Vidly/Controllers/MovieController.cs
@@ -26,13 +26,44 @@ namespace Vidly.Controllers
 		// GET: Movies
 		public ActionResult Index()
 		{
-			var movies = 
+			var movies =
 				//get list of movies in Index view
 				_context.Movies.Include(m => m.Genres).ToList();
 			return View(movies);
 		}
 
-		
+		public ActionResult New()
+		{
+			var viewModel = new MovieFormViewModel()
+			{
+				Genres = _context.Genres.ToList()
+			};
+
+			return View("MovieForm", viewModel);
+		}
+
+		[HttpPost]
+		public ActionResult Save(Movie movie)
+		{
+			if (movie.Id == 0)
+			{
+				movie.DateAdded = DateTime.Now;
+				_context.Movies.Add(movie);
+			}
+			else
+			{
+				var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+
+				movieInDb.Name = movie.Name;
+				movieInDb.DateReleased = movie.DateReleased;
+				movieInDb.GenreId = movie.GenreId;
+				movieInDb.NumberInStock = movie.NumberInStock;
+			}
+
+			_context.SaveChanges();
+
+			return RedirectToAction("Index", "Movie");
+		}
 
 		public ActionResult Details(int id)
 		{
@@ -41,6 +72,21 @@ namespace Vidly.Controllers
 				.Include(m => m.Genres)
 				.SingleOrDefault(m => m.Id == id);
 			return View(movieById);
+		}
+
+		public ActionResult Edit(int id)
+		{
+			var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+			if (movie == null) return HttpNotFound();
+
+			var viewModel = new MovieFormViewModel()
+			{
+				Movie = movie,
+				Genres = _context.Genres.ToList()
+			};
+
+			return View("MovieForm", viewModel);
 		}
 
 		[Route("movie/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
--- Vidly_with_account_controller/Vidly/Vidly/Models/Movie.cs
+++ Vidly_with_account_controller/Vidly/Vidly/Models/Movie.cs
@@ -11,12 +11,17 @@ namespace Vidly.Models
 		public int Id { get; set; }
 		public string Name { get; set; }
 
-		[Required]
 		public Genre Genres { get; set; }
-		public int GenreId { get; set; }
 
+		[Display(Name = "Genre")]
+		[Required]
+		public int GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
+
+		[Display(Name = "Release Date")]
 		public DateTime DateReleased { get; set; }
+
+		[Display(Name = "Number in Stock")]
 		public int NumberInStock { get; set; }
 	}
 }
