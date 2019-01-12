8e26451a6032052ea49b5a7e5472547e60620c09
Adisa Mrvoljak <adisa.mrvoljak@live.com>
2019-01-12 14:44:19 +0100

    add migration ChangeStringLengthOfCustomerName

--- Vidly_with_account_controller/Vidly/Vidly/Models/Customer.cs
+++ Vidly_with_account_controller/Vidly/Vidly/Models/Customer.cs
@@ -11,6 +11,7 @@ namespace Vidly.Models
 		public int Id { get; set; }
 
 		[Required]
+		[StringLength(255)]
 		public string Name { get; set; }
 
 		public bool IsSubscribedToNewsletter { get; set; }
