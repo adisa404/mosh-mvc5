197b96bc5b4a9edc98beee879075b308b9041939
Adisa Mrvoljak <adisa.mrvoljak@live.com>
2019-01-13 17:35:24 +0100

    building an api

--- Vidly_with_account_controller/Vidly/Vidly/Global.asax.cs
+++ Vidly_with_account_controller/Vidly/Vidly/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace Vidly
 {
@@ -12,7 +13,8 @@ namespace Vidly
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+			AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
